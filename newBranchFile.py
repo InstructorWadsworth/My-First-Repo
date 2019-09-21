562e24d8ed38d58c7e6c8ba5e8d53a11dc34d0c9
Nick Wadsworth <nicholaswadsworth@boisestate.edu>
2019-09-21 13:57:28 -0600

    added print statement

--- newBranchFile.py
+++ newBranchFile.py
@@ -1,2 +1,3 @@
 print("hello")
 print("I'm adding a change to see if I can make it show up by using git fetch")
+print("I figured out why I was having trouble downloading the new changes from the orginal repo.  It turns out that I set my upstream URL to my InstructorWadsworth repo instead of the original repo.")
